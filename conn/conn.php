1e5a9ce30ff02d21a9239360d7b3c6a69aa0da0b
賀旭超 <slepox@126.com>
2018-06-04 00:12:31 +0800

    Update conn.php

--- conn/conn.php
+++ conn/conn.php
@@ -1,5 +1,5 @@
 <?php
- //$link = mysqli_connect("localhost","a0915104520","7562149","a0915104520",3306) or die("数据库服务器连接错误".mysql_error());
+ //$link = mysqli_connect("localhost","a0915104520","密码","a0915104520",3306) or die("数据库服务器连接错误".mysql_error());
  $link = mysqli_connect("localhost","root","","a0915104520",3306) or die("数据库服务器连接错误".mysql_error());
  mysqli_query($link,"set names utf8");
 ?>
